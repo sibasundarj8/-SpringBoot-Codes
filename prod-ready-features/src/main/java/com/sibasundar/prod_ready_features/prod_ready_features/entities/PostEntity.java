290ec473455dcab30f317d23bafe3030350064a3
Sibasundar Jena <sibasundarj8@gmail.com>
2026-03-18 22:17:55 +0530

    prototype

---
+++ prod-ready-features/src/main/java/com/sibasundar/prod_ready_features/prod_ready_features/entities/PostEntity.java
@@ -0,0 +1,36 @@
+package com.sibasundar.prod_ready_features.prod_ready_features.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PostEntity extends AuditableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @PrePersist
+    void beforeSave() {
+
+    }
+
+    @PreUpdate
+    void beforeUpdate() {
+
+    }
+
+    @PreRemove
+    void beforeDelete() {
+
+    }
+
+
+}
---
+++ prod-ready-features/src/main/java/com/sibasundar/prod_ready_features/prod_ready_features/services/PostService.java
@@ -0,0 +1,16 @@
+package com.sibasundar.prod_ready_features.prod_ready_features.services;
+
+import com.sibasundar.prod_ready_features.prod_ready_features.dto.PostDTO;
+
+import java.util.List;
+
+public interface PostService {
+
+    List<PostDTO> getAllPosts();
+
+    PostDTO createNewPost(PostDTO inputPost);
+
+    PostDTO getPostById(Long postId);
+
+    PostDTO updatePost(PostDTO inputPost, Long postId);
+}
